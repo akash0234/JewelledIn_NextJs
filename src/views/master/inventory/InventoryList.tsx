c50fca323e126a50436e65debe069d477b8d6a4d
amitava bhowmick <akash0234@gmail.com>
2023-12-13 01:17:43 +0530

    Update InventoryList.tsx

--- src/views/master/inventory/InventoryList.tsx
+++ src/views/master/inventory/InventoryList.tsx
@@ -12,21 +12,22 @@ import Skeleton from "react-loading-skeleton"
 import { useDispatch, useSelector } from "react-redux"
 import { AppDispatch, RootState } from "src/store"
 import { Prdmst } from "src/types/inventory/InventoryListType"
-import { fetchData, fetchInventoryData } from 'src/store/masters/inventory'
-
-
+import { fetchData } from 'src/store/masters/inventory'
 
 const InventoryList = () => {
     // ** State
     const [selectedIndex, setSelectedIndex] = useState<number>(1)
 
-    // const { id } = router.query;
+    const handleListItemClick = (index: number) => {
+        console.log(index)
+        setSelectedIndex(index)
+    }
 
 
     // ** Hooks
     const dispatch = useDispatch<AppDispatch>()
     const store = useSelector((state: RootState) => state.inventory)
-    console.log(store.data)
+
     useEffect(() => {
         dispatch(
             fetchData()
@@ -34,16 +35,7 @@ const InventoryList = () => {
 
     }, [dispatch])
 
-    const handleListItemClick = async (invcod: string) => {
 
-
-        const result = await dispatch(fetchInventoryData(invcod));
-        const inventoryDetails = result.payload as Prdmst
-        console.log(inventoryDetails)
-        //setSelectedIndex(index)
-    }
-
-    /*Check If Store Has component Data */
 
     return (
         <>
@@ -56,9 +48,8 @@ const InventoryList = () => {
 
             }}>
                 {store.data.map((item: Prdmst, index: number) => (
-
                     <ListItem disablePadding key={index} >
-                        <ListItemButton selected={selectedIndex === 0} onClick={() => handleListItemClick(item.invcod)} sx={{
+                        <ListItemButton selected={selectedIndex === 0} onClick={() => handleListItemClick(index)} sx={{
                             '&:after': {
                                 height: '1px', content: '""', width: 'Calc(100% - 30px)', position: 'absolute', bottom: 0, bgcolor: '#cccccc73'
                             }
@@ -79,7 +70,6 @@ const InventoryList = () => {
             </List>
         </>
     )
-
 
 }
 export default InventoryList
